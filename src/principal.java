13c807240bebb9e8c8fb2878e560f211480c07e7
KRON05FD <105858747+KRON05FD@users.noreply.github.com>
2023-05-02 15:00:05 -0500

    inicioproyecto
    se carga la clase principal

---
+++ src/principal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class principal {
+
+    public static void main(String[] args) {
+        int limite, i, j, k, entrada, ingresados;
+        String busqueda;
+        boolean bandera;
+        limite = 10;
+        bandera = false;
+        entrada = 0;
+        ingresados = 0;
+
+        Scanner consola new = consola(System.in);
+
+
+    }
+
+}
