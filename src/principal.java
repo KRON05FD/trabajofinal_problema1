247a60b012c71e78088938997a1a7bcfdc10a2f3
KRON05FD <105858747+KRON05FD@users.noreply.github.com>
2023-09-03 11:37:48 -0500

    cambiosmac
    cambios mac

--- src/principal.java
+++ src/principal.java
@@ -1,21 +1,27 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class principal {
 
     public static void main(String[] args) {
-        int limite, i, j, k, entrada, ingresados; //Declaracion de variables
+        int limite;  //Declaracion de variables
+        int i;
+        int j;
+        int k;
+        int entrada;
+        int ingresados;
+        int opcion;
         String busqueda;
         Boolean bandera;
 
         limite = 0; //inicializar variables
+        opcion = 0;
         bandera = false;
-        entrada = 0;
         ingresados = 0;
 
         Scanner consola = new Scanner(System.in);
-        while (!bandera){
+        while (!bandera) {
             System.out.println("### Bienvenido Promedico Soft ###");
-            System.out.println("Selecciona una opcion");
             System.out.println("1. Registrar historia clinica paciente");
             System.out.println("2. Consultar historia clinica paciente");
             System.out.println("3. Consultar fecha ultima visita");
@@ -24,17 +30,25 @@ public class principal {
             System.out.println("6. Reporte general de pacientes");
             System.out.println("7. Terminar");
 
-        }
-
-
-
-        int entrada = consola.nextLine();
-
-
-        switch (entrada) {
-
-            case 1:
-                System.out.println("Seleccione la opcion # 1: ");
+            try {
+                System.out.println("Selecciona una opcion");
+                opcion = Integer.parseInt(consola.nextLine());
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Selecion opcion 1");
+                        break;
+                    case 2:
+                        System.out.println("seleccion opcion 2");
+                        break;
+                    case 7:
+                        bandera = false;
+                    default:
+                        System.out.println("Opcion incorrecta");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un numero");
+                consola.next();
+            }
 
         }
     }
